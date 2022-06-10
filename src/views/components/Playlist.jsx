7ea38c9c732911ccfccfe8215d0e1e7961234386
lainshiHenry <henryle913@outlook.com>
2022-06-10 10:09:46 -0500

    Initial commit with basic app layout

---
+++ src/views/components/Playlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+
+const Playlist = () => {
+    return (
+        <div className='container '>
+            Player
+
+        </div>
+    )
+}
+
+export default Playlist
